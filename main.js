a7ce34984ce92046a8e64fee3f019f105b2d973f
tomas <72792707+tomas-land@users.noreply.github.com>
2021-09-01 19:51:19 +0300

    initials

--- main.js
+++ main.js
@@ -6,24 +6,16 @@ links.forEach((link) => {
     link.addEventListener('click', () => {
 
         const el = document.getElementById(link.getAttribute("data-link"));
+        el.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
 
-        link.classList.contains('mywork') ?
-            window.scrollTo({
-                top: 2300,
-                behavior: 'smooth'
-            }) :
-            el.scrollIntoView({
-                behavior: 'smooth',
-                block: 'center'
-            });
         let slide_menu = document.querySelector('.slide-menu');
         slide_menu.classList.toggle("slide-menu-active")
         hamburger.classList.toggle("is-active");
     })
 });
-
-
-
 
 hamburger.addEventListener("click", function () {
     hamburger.classList.toggle("is-active");
