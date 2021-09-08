49a9b39e15fe014c4bb265ce365b64a59c8b7717
tomas <72792707+tomas-land@users.noreply.github.com>
2021-09-08 10:07:22 +0300

    initials

--- main.js
+++ main.js
@@ -21,3 +21,19 @@ hamburger.addEventListener("click", function () {
     hamburger.classList.toggle("is-active");
     slide_menu.classList.toggle("slide-menu-active")
 });
+
+
+let phone_icon= document.querySelector('.phone-icon');
+let email_icon= document.querySelector('.email-icon');
+let hidden_phone= document.querySelector('.hidden-phone');
+let hidden_email= document.querySelector('.hidden-email');
+
+phone_icon.addEventListener('click',(e)=>{
+hidden_phone.classList.toggle('hidden-phone-active')
+hidden_email.classList.remove('hidden-email-active')
+})
+email_icon.addEventListener('click',(e)=>{
+hidden_email.classList.toggle('hidden-email-active')
+hidden_phone.classList.remove('hidden-phone-active')
+
+})
