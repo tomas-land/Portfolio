63f1f8e61e27946acb1d20250396d054d2abacd3
tomas <72792707+tomas-land@users.noreply.github.com>
2021-09-08 21:20:07 +0300

    initials

--- main.js
+++ main.js
@@ -23,17 +23,34 @@ hamburger.addEventListener("click", function () {
 });
 
 
-let phone_icon= document.querySelector('.phone-icon');
-let email_icon= document.querySelector('.email-icon');
-let hidden_phone= document.querySelector('.hidden-phone');
-let hidden_email= document.querySelector('.hidden-email');
+let phone_icon = document.querySelector('.phone-icon');
+let email_icon = document.querySelector('.email-icon');
+let hidden_phone = document.querySelector('.hidden-phone');
+let hidden_email = document.querySelector('.hidden-email');
 
-phone_icon.addEventListener('click',(e)=>{
-hidden_phone.classList.toggle('hidden-phone-active')
-hidden_email.classList.remove('hidden-email-active')
+phone_icon.addEventListener('click', (e) => {
+    hidden_phone.classList.toggle('hidden-phone-active')
+    hidden_email.classList.remove('hidden-email-active')
+    phone_icon.classList.remove('tada'); // reset animation
+    void phone_icon.offsetWidth; // trigger reflow
+    phone_icon.classList.add('tada'); // start animation
+    hidden_phone.classList.add('fadeInLeft');
+    // hidden_phone.classList.toggle('fadeOutRight');
+    // if (hidden_phone.classList.contains('fadeInLeft')) {
+        // hidden_phone.classList.remove('fadeInLeft');
+        hidden_phone.classList.toggle('fadeOutRight');
+       
+    // } else{
+    //     hidden_phone.classList.add('fadeInLeft');
+    // }
+
+
 })
-email_icon.addEventListener('click',(e)=>{
-hidden_email.classList.toggle('hidden-email-active')
-hidden_phone.classList.remove('hidden-phone-active')
-
+email_icon.addEventListener('click', (e) => {
+    hidden_email.classList.toggle('hidden-email-active')
+    hidden_phone.classList.remove('hidden-phone-active')
+    email_icon.classList.remove('tada'); // reset animation
+    void email_icon.offsetWidth; // trigger reflow
+    email_icon.classList.add('tada'); // start animation
+    hidden_email.classList.add('fadeInLeft');
 })
